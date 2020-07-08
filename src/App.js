6e96fa26c4151fac937641153201e31de5e40918
Anh Thang Bui <buianhthang89@gmail.com>
2020-07-08 09:31:55 +0700

    :construction: change alpha, mod and some override key color

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import * as fetch from 'node-fetch';
-import { Select } from 'antd';
+import { Select, Checkbox } from 'antd';
+
 import 'antd/dist/antd.css';
-// import '../src/scss/sp-abs.scss';
 import './colorway.css';
+import alphaCodes from './key_alpha.json';
+import modCodes from './key_mod.json';
+import layer from './layer.json';
+import override from './override.json';
 
 const { Option } = Select
 
@@ -13,15 +17,16 @@ const colorways = ['SA Carbon', 'GMK 8008', 'GMK Bento', 'SA Vilebloom']
 
 function App() {
   const [keyboardNames, setKeyboardNames] = useState([])
-  const [layoutNames, setLayoutNames] = useState([])
-  const [layouts, setLayouts] = useState({})
+  const [keyboard, setKeyboard] = useState({})
   const [keymaps, setKeymaps] = useState([])
-  const [width, setWidth] = useState(0)
-  const [colorway, setColorway] = useState("gmk carbon")
+  const [clw, setClw] = useState("gmk carbon")
+  const [modClw, setModClw] = useState("")
+
   useEffect(() => {
     fetch('https://api.qmk.fm/v1/keyboards')
       .then(res => res.json())
       .then(res => {
+        res = res.filter(r => r.startsWith("percent") || r.startsWith("exclusive") || r.startsWith("duck"))
         setKeyboardNames(res)
       })
   }, [])
@@ -30,18 +35,22 @@ function App() {
     fetch(`https://api.qmk.fm/v1/keyboards/${keyboard_name}`)
       .then(res => res.json())
       .then(res => {
-        setLayoutNames(Object.keys(res.keyboards[keyboard_name].layouts))
-        setLayouts(res.keyboards[keyboard_name].layouts)
-        setWidth(res.keyboards[keyboard_name].width)
+        setKeyboard(res.keyboards[keyboard_name])
       })
   }
 
   const chooseLayout = (name) => {
-    setKeymaps(layouts[name].layout)
+    setKeymaps(keyboard.layouts[name].layout)
   }
 
   const changeColorway = (name) => {
-    setColorway(name.toLowerCase().replace(' ', '-') + '-key')
+    setClw(name.toLowerCase().replace(' ', '-'))
+    if (!modClw) {
+      setModClw(name.toLowerCase().replace(' ', '-'))
+    }
+  }
+  const changeModColorway = (name) => {
+    setModClw(name.toLowerCase().replace(' ', '-'))
   }
 
   return (
@@ -57,7 +66,7 @@ function App() {
         </Select>
         <Select style={{ width: 300 }} onSelect={chooseLayout}>
           {
-            layoutNames.map(layout => {
+            keyboard.layouts && Object.keys(keyboard.layouts).map(layout => {
               return <Option value={layout} key={layout}>{layout}</Option>
             })
           }
@@ -69,14 +78,41 @@ function App() {
             })
           }
         </Select>
-
-        <div style={{position: 'relative', width: 60 * width }}>
+        <Select style={{ width: 300 }} onChange={changeModColorway}>
+          {
+            colorways.map(clw => {
+              return <Option value={clw} key={clw}>{clw}</Option>
+            })
+          }
+        </Select>
+        {/* <Checkbox checked={false} onClick={() => alert("fuck")}>Different Modifiers Colorway</Checkbox> */}
+        <div style={{
+          position: 'relative',
+          width: 60 * (keyboard.width + 1),
+          height: 60 * (keyboard.height - 1),
+          // border: '1px solid white'
+        }}>
           {keymaps.map((k, idx) => {
+            const keyCode = layer[keyboard.keyboard_folder] ? layer[keyboard.keyboard_folder][idx] : k.label
+            let suffix
+            if (override[clw] && override[clw][keyCode]) {
+              suffix = override[clw][keyCode]
+            } else if (modCodes.includes(keyCode)) {
+              suffix = 'mod'
+            } else {
+              suffix = 'key'
+            }
+
+            let className = modCodes.includes(keyCode)
+              ? `${modClw}-${suffix}`
+              : `${clw}-${suffix}`
+
             return <div
               key={idx}
               id={idx}
               title={k.label}
-              className={colorway}
+              className={className}
+              key-code={keyCode}
               style={{
                   borderRadius: `6px`,
                   boxShadow: `inset 0 -1px 0 3px rgba(0,0,0,.1), 0 0 0 1px rgba(0,0,0,.3)`,
@@ -86,8 +122,8 @@ function App() {
                   fontSize: "small",
                   left: `${k.x * 60}px`,
                   top: `${k.y * 60}px`,
-                  width: `${(k.w || 1) * 60}px`,
-                  height: `${(k.h || 1) * 60}px`,
+                  width: `${(k.w || 1) * 60 - 5}px`,
+                  height: `${(k.h || 1) * 55}px`,
                   position: `absolute`,
                   marginRight: `5px`,
                   marginBottom: `5px`
