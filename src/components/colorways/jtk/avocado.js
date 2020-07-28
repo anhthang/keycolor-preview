73b78e23f74cd09d81445729901e2c7da3b55bc6
Anh Thang Bui <buianhthang89@gmail.com>
2020-07-28 10:00:58 +0700

    :bento: JTK Avocado

---
+++ src/components/colorways/jtk/avocado.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'jtk-avocado',
+  kits: {
+    yellow: {},
+    green: {},
+  },
+  override: {
+    KC_ESC: 'accent',
+    KC_GESC: 'accent',
+    KC_ENT: 'accent',
+    KC_PENT: 'accent',
+    KC_UP: 'accent',
+    KC_DOWN: 'accent',
+    KC_LEFT: 'accent',
+    KC_RGHT: 'accent',
+  }
+}
--- src/components/colorways/jtk/index.js
+++ src/components/colorways/jtk/index.js
@@ -1,7 +1,9 @@
+import jtk_avocado from './avocado';
 import jtk_classic_fc from './classic_fc';
 import jtk_suited_assassin from './suited_assassin';
 
 export default [
+    jtk_avocado,
     jtk_classic_fc,
     jtk_suited_assassin,
 ]
